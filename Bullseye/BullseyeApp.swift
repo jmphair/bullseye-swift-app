05904797d8e08d5e352f936b5b0cb2f78f4e93a5
Jonathan Phair <jmphair@gmail.com>
2023-04-06 16:03:30 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Jonathan Phair on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
