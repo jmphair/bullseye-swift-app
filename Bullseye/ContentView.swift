ec3763a621fa0cf57b22cb8c52e221371a66ef1b
Jonathan Phair <jmphair@gmail.com>
2023-04-06 16:37:31 -0400

    add: button

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            VStack {
 
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-            Text("89")
-            Slider(value: .constant(50), in: 1.0...100.0)
-            
-            
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                Text("89")
+                HStack {
+                    Text("1")
+                    Slider(value: .constant(50), in: 1.0...100.0)
+                    Text("100")
+                }
+                
+                
+            }
+            Button(action: {}) {
+                Text("Hit me")
+            }
         }
     }
 }
