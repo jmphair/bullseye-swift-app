efcf46c4782f4020f53f8c934b87c2762ab64fdb
Jonathan Phair <jmphair@gmail.com>
2023-04-07 11:01:22 -0400

    add: landscape view

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,29 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            VStack {
-
-                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                Text("89")
-                HStack {
-                    Text("1")
-                    Slider(value: .constant(50), in: 1.0...100.0)
-                    Text("100")
-                }
-                
-                
-            }
-            Button(action: {}) {
-                Text("Hit me")
-            }
+  var body: some View {
+    VStack {
+      VStack {
+        
+        Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        Text("89")
+        HStack {
+          Text("1")
+          Slider(value: .constant(50), in: 1.0...100.0)
+          Text("100")
         }
+        Button(action: {}) {
+          Text("Hit me")
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+    ContentView()
+      .previewLayout(.fixed(width: 568, height: 320))
+  }
 }
